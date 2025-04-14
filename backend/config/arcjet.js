129df0e6259cb76807cd58413c1c0790e91d7880
SebMZI <mrgy.sebastien@gmail.com>
2025-04-14 18:11:52 +0200

    feat(backend): modify refill rate for Arcjet token bucket

--- backend/config/arcjet.js
+++ backend/config/arcjet.js
@@ -13,8 +13,8 @@ const aj = arcjet({
     }),
     tokenBucket({
       mode: "LIVE",
-      refillRate: 5, // Refill 5 tokens per interval
-      interval: 10, // Refill every 10 seconds
+      refillRate: 1, // Refill 5 tokens per interval
+      interval: 30, // Refill every 10 seconds
       capacity: 10, // Bucket capacity of 10 tokens
     }),
   ],
