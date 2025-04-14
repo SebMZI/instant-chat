e430a0c238ce4c3317f7c8da0de7250dfd451b27
SebMZI <mrgy.sebastien@gmail.com>
2025-04-14 14:38:34 +0200

    feat: add backend structure

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from "express";
+import cookieparser from "cookie-parser";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+const app = express();
 app.use(express.json());
+app.use(cookieparser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/", (req, res) => {
+  res.send("Test successful");
+});
 
-module.exports = app;
+app.listen(5580, () => {
+  console.log(`Server listening on http://localhost:${5580}`);
+});
